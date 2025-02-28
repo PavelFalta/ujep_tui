0e9dd00e4d8b687a1fadac0286021e9929f45dcb
PavelFalta <paja.falta@gmail.com>
2025-02-28 22:18:01 +0000

    Modify default_year to be parsed as i64 and converted to string, and pass it as a reference to fetch_timetable_data function

--- src/fetch_timetable.rs
+++ src/fetch_timetable.rs
@@ -71,10 +71,10 @@ pub async fn fetch_timetable() -> Result<(), Box<dyn std::error::Error>> {
     let profile_response: serde_json::Value = serde_json::from_str(&profile_data)?;
 
     let stagid = profile_response["data"]["roles"]["student"][0]["roleId"].as_str().unwrap_or_default();
-    let default_year = profile_response["data"]["years"]["defaultYear"].as_str().unwrap_or_default();
+    let default_year = profile_response["data"]["years"]["defaultYear"].as_i64().unwrap_or_default().to_string();
     println!("Fetching timetable for stagid {} and year {}", stagid, default_year);
     
-    let timetable_response = fetch_timetable_data(&client, &headers, stagid, default_year).await?;
+    let timetable_response = fetch_timetable_data(&client, &headers, stagid, &default_year).await?;
 
     save_timetable_to_file(&timetable_response)?;
 
